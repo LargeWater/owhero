4ebd903774bea03fc42e982e55c9b4d9b14696be
Nicholas Bailey <nickcyg1992@gmail.com>
2022-08-24 09:37:34 -0500

    create .env

--- owhero/settings.py
+++ owhero/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bgdusy%yt!4%-pe86b$arw^0r+@tvj@5ky44q8k#=^=lc%q5_g'
+# 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
